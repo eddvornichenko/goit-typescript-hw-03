95752a7c40ee0d6e3a2e034fe4319e7e4f37ee29
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-11-23 22:45:22 +0200

    task-done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,60 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+    constructor(key: Key) {
+      this.door = false;
+    }
+    comeIn(person: Person) {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log("Open door!");
+      } else {
+        console.log("Closed door!");
+      }
+    }
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log("Open door!");
+      } else {
+        console.log("Closed door!");
+      }
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
+  export {};
